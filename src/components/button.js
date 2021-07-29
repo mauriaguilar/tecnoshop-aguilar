6cea70dab1a80128a73aceb2e58eb6dbc4be0114
Mauricio Aguilar <mauricio.aguilar@incluit.com>
2021-07-29 19:08:40 -0300

    Fixing styles

--- src/components/button.js
+++ src/components/button.js
@@ -1,10 +1,10 @@
 import './button.css';
 
-const Button = (props) => {
+const Button = ({text}) => {
     return (
         <li className="nav-item" role="presentation">
             <button className="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">
-                {props.text}
+                {text}
             </button>
         </li>
     );
