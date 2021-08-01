f3d442327ccba3993c30140186e0bfbe574325b9
Mauricio Aguilar <mauricio.aguilar@incluit.com>
2021-08-01 16:38:43 -0300

    Adding CartWidget component

--- src/components/title.js
+++ src/components/title.js
@@ -1,10 +1,16 @@
 import './title.css';
+import CartWidget from "./CartWidget"
 
 const Title = () => {
     return (
         <>
             <div className="row">
-                    <div id="title" className="col d-flex justify-content-center">TecnoShop Aguilar</div>
+                    <div id="title" className="col d-flex justify-content-center">
+                        <span className="d-flex align-items-center">
+                            <CartWidget />
+                        </span>
+                        TecnoShop Aguilar
+                    </div>
             </div>
             <div className="row">
                     <div id="subtitle" className="col d-flex justify-content-center">Technology items for your life.</div>
