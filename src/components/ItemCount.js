41f14e315d37df3964329292109a38bb1b2e56e9
Mauricio Aguilar <mauricio.aguilar@incluit.com>
2021-08-03 21:53:41 -0300

    Adding logic

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,20 +1,21 @@
 import './ItemCount.css';
 
-const ItemCount = ({ greeting }) => {
+const ItemCount = ({ stock, initial, onAdd }) => {
     return (
         <>
-            <div class="card">
-                <img src="http://lorempixel.com/400/200/technics/" class="card-img-top" alt="..." />
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div className="card">
+                <img src="http://lorempixel.com/400/200/technics/" className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 </div>
-                <div class="card-footer">
-                    <small class="text-muted">
+                <div className="card-footer">
+                    <small className="text-muted">
+
                         {/* Stock controls component */}
                         <div className="input-group mb-3">
                             <button className="btn btn-outline-secondary m-0" type="button" id="button-addon1">-</button>
-                            <input type="text" className="form-control" placeholder="0" aria-label="amount" aria-describedby="button-addon1" />
+                            <input type="text" className="form-control" placeholder={initial} aria-label="amount" aria-describedby="button-addon1" />
                             <button className="btn btn-outline-secondary m-0" type="button" id="button-addon2">+</button>
                         </div>
 
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,16 +1,20 @@
 import './ItemListContainer.css';
 import ItemCount from "./ItemCount"
 
+const onAdd = (num_of_items) => {
+    console.log("Adding " + num_of_items + " to cart.")
+}
+
 const ItemListContainer = ({ greeting }) => {
     return (
         <div className="row m-5 border border-dark">
             <div id="itemListContainer" className="col fs-3 ">
                 Hi <span className="greeting">{ greeting }</span>,
                 these are our innovative products!
-                <div class="card-group">
-                    <ItemCount />
-                    <ItemCount />
-                    <ItemCount />
+                <div className="card-group">
+                    <ItemCount stock="5" initial="1" onAdd="onAdd()"/>
+                    <ItemCount stock="5" initial="1" onAdd="onAdd()"/>
+                    <ItemCount stock="5" initial="1" onAdd="onAdd()"/>
                 </div>
             </div>
         </div>
