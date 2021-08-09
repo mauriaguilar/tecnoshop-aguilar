958654c19fbdaa5bdc67aaf14524f7ac53a691d3
Mauricio Aguilar <mauricio.aguilar@incluit.com>
2021-08-09 00:46:40 -0300

    Adding logic to Entrega4

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,6 +1,38 @@
 import './ItemCount.css';
+import React, {useState} from 'react';
+
+
 
 const ItemCount = ({ stock, initial, onAdd }) => {
+
+    const [count, setCount] = useState(initial);
+
+    const name = "speaker";
+
+    const delItem = () => {
+        console.log("Deleting item...");
+        if (count !== 0)
+            setCount(count - 1);
+        else
+            console.log("It is not possible to delete an item. Count is 0.");
+    }
+        
+    const addItem = () => {
+        console.log("Adding item...");
+        if (count < stock)
+            setCount(count + 1);
+        else
+            console.log("It is not possible to add a new item. Stock is " + stock + ".");
+    }
+
+    const addToCart = () => {
+        console.log("Adding item to cart...");
+        if (stock > 0)
+            onAdd({"name": name, "count": count});
+        else
+            console.log("It is not possible to add an item to cart, because stock is 0.");
+    }
+
     return (
         <>
             <div className="card">
@@ -16,15 +48,21 @@ const ItemCount = ({ stock, initial, onAdd }) => {
 
                         {/* Stock controls component */}
                         <div className="input-group mb-3">
-                            <button className="btn btn-outline-secondary m-0" type="button" id="button-addon1">-</button>
-                            <input type="text" className="form-control text-center" placeholder={initial} aria-label="amount" aria-describedby="button-addon1" />
-                            <button className="btn btn-outline-secondary m-0" type="button" id="button-addon2">+</button>
+                            <button className="btn btn-outline-secondary m-0" type="button" id="button-addon1" onClick={delItem}>-</button>
+                            <input type="text" className="form-control text-center" placeholder={count} aria-label="amount" aria-describedby="button-addon1" />
+                            <button className="btn btn-outline-secondary m-0" type="button" id="button-addon2" onClick={addItem}>+</button>
                         </div>
 
                         {/* Add-to-cart button component */}
                         <div className="row">
                             <div className="col d-flex justify-content-center">
-                                    <button className="btn btn-outline-secondary m-0 " type="button" id="button-addon2">Agregar al carrito</button>
+                                    <button
+                                        className="btn btn-outline-secondary m-0"
+                                        type="button"
+                                        id="button-addon2"
+                                        onClick={addToCart}>
+                                            Agregar al carrito
+                                    </button>
                             </div>
                         </div>
 
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,18 +1,27 @@
 import './ItemListContainer.css';
 import ItemCount from "../ItemCount/ItemCount"
 
-const onAdd = (num_of_items) => {
-    console.log("Adding " + num_of_items + " to cart.")
-}
 
 const ItemListContainer = ({ greeting }) => {
+    let cart = {};
+
+    const onAdd = (item) => {
+        console.log("Adding item to cart.");
+        if (item.name in cart)
+            cart[item.name] += item.count;
+        else
+            cart[item.name] = item.count;
+        console.log("Cart:");
+        console.log(cart);
+    }
+
     return (
         <div className="row m-5 border border-dark">
             <div id="itemListContainer" className="col fs-3 ">
                 Hi <span className="greeting">{ greeting }</span>,
                 these are our innovative products!
                 <div className="card-group">
-                    <ItemCount stock="5" initial="1" onAdd="onAdd()"/>
+                    <ItemCount stock={5} initial={1} onAdd={onAdd}/>
                 </div>
             </div>
         </div>
