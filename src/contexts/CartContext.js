5c637e76b8751c39760cb57dd31f85df0d85f616
Mauricio Aguilar <mauricio.aguilar@incluit.com>
2021-08-23 00:21:58 -0300

    Adding removeItem, clear, isInCart functions

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -13,37 +13,50 @@ export default function CartProvider({ children }) {
 
     const addItem = (item_id, quantity) => {
         console.log("Adding " + quantity + " items of " + item_id + ".");
-        console.log({...cart})
         cart[item_id] = quantity;
         setCart({...cart});
         console.log("---cart-----");
         console.log(cart);
         console.log("---/cart-----");
+
+        // Testing functions
+        // removeItem(item_id);
+        // clear();
+        // isInCart(item_id);
+        // isInCart(0);
     }
 
-    // const removeItem = (item_id) => {
-    //     console.log("Removing item id.");
-    // }
+    const removeItem = (item_id) => {
+        console.log("Removing item id.");
+        if (isInCart(item_id)){
+            delete cart[item_id];
+            setCart({...cart});
+            console.log("Item " + item_id + " was removed.");
+        } else {
+            console.log("It is not possible to remove the item because it doesn't exist.")
+        }
+    }
 
-    // const clear = () => {
-    //     console.log("Removing all items.");
-    // }
+    const clear = () => {
+        console.log("Removing all items.");
+        setCart({});
+    }
 
-    // const isInCart = (item_id) => {
-    //     console.log("Is in cart.");
-    // }
+    const isInCart = (item_id) => {
+        console.log("Is in cart: " + ((item_id in cart) ? true : false));
+        return (item_id in cart) ? true : false;
+    }
 
     return (
         <CartContext.Provider value={{
             cart: cart,
-            addItem: addItem
+            addItem: addItem,
+            removeItem: removeItem,
+            clear: clear,
+            isInCart: isInCart
         }}>
             {children}
         </CartContext.Provider>
     )
-    // ,
-    // removeItem: removeItem,
-    // clear: clear,
-    // isInCart: isInCart
 
 }
